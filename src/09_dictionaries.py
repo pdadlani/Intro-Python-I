ea4956a9b01f8daff3a243ec51d8541e8bffb14b
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-12-03 09:44:16 -0800

    fixed silly bug in 04 - printing.

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,18 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+print(waypoints)
+waypoints.append({"lat": 40, "lon": -120, "name": "a new place"})
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for place in waypoints:
+    print(place.values())
