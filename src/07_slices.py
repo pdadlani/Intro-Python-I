275fb4a6f84703be6209e9057f1b774cd1b9fb1f
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-04-06 13:30:56 -0700

    finished modules, print, lists, tuples, and slices.

--- src/07_slices.py
+++ src/07_slices.py
@@ -21,9 +21,8 @@ print(a[-2])
 print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
-alen = len(a)
-middle = alen//2
-print(a[(middle-1): middle+1])
+mid = len(a)//2
+print(a[mid-1:mid+1])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
 print(a[1:])
@@ -37,4 +36,4 @@ s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
 print(s[7:12])
-print(s[slice(7, 12)])
+print(s[slice(7,12)])
--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,13 +4,13 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
-def changeX():
+def change_x():
     global x
     x = 99
 
-changeX()
+change_x()
 
-# This prints 12. What do we have to modify in changeX() to get it to print 99?
+# This prints 12. What do we have to modify in change_x() to get it to print 99?
 print(x)
 
 
